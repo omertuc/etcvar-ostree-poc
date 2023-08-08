62fa9d4136feb6d56af7f79099966de4509dc51d
Javi Polo <jpolo@redhat.com>
2023-08-08 20:18:27 +0200

    Use /var/tmp to avoid ostree unlock

--- commands.sh
+++ commands.sh
@@ -5,28 +5,25 @@ sudo systemctl stop kubelet
 crictl ps -q | xargs crictl stop
 sudo systemctl stop crio
 
-# Unlock (can we just tar into somewhere else instead?)
-ostree admin unlock
-
 # Prepare backup folder
-mkdir /usr/bkup/
+mkdir /var/tmp/backup
 
 # Tar var
 # TODO: Exclude CNI binaries /var/lib/cni
 tar -czf \
-    /usr/bkup/ocpvar.tar.gz \
-    --exclude='/var/tmp/' \
-    --exclude='/var/lib/log' \
-    --exclude='/var/lib/containers' \
+    /var/tmp/backup/ocpvar.tar.gz \
+    --exclude='/var/tmp/*' \
+    --exclude='/var/lib/log/*' \
+    --exclude='/var/lib/containers/*' \
     /var/
     
 
 # Tar etcd
 # TODO: Different treatment for M and A files?
-ostree admin config-diff | awk '{print "/etc/" $2}' | xargs tar -czf /usr/bkup/etcd.tar.gz
+ostree admin config-diff | awk '{print "/etc/" $2}' | xargs tar -czf /var/tmp/backup/etc.tar.gz
 
 # Commit
-ostree commit --branch haha /usr/bkup
+ostree commit --branch haha /var/tmp/backup
 
 # Into image
 mkdir /root/.docker
@@ -39,7 +36,7 @@ podman save quay.io/otuchfel/ostmagic:latest -o ha.tar
 
 # vm stuff
 mount /sysroot -o remount,rw
-ostree container unencapsulate registry:quay.io/otuchfel/ostmagic:latest
-ostree refs --create=haha <commit_hash>
+ostree container unencapsulate registry:quay.io/otuchfel/ostmagic:latest | tee /tmp/unencapsulate.backup
+ostree refs --create=haha $(cut -d ' ' -f 2 < /tmp/unencapsulate.backup)
 ostree cat haha /etc.tar.gz | tar -C / -xz
 ostree cat haha /ocpvar.tar.gz | tar -C / -xz
